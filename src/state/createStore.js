b060e86f54ab1ac08c18be101f133fa910981ca5
Christian <christian@stabmag.com>
2018-08-25 12:58:16 -0400

    removes redux dev tools

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -92,6 +92,6 @@ const createStore = () =>
   reduxCreateStore(
     reducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 export default createStore;
